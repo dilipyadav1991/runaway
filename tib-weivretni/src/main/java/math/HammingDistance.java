3219ecf7b22e0cd9838aa4e883154d18177589a6
Dilip Yadav <dyadav@reputation.com>
2018-05-07 21:33:20 +0530

    Hamming distance

--- tib-weivretni/src/main/java/math/HammingDistance.java
+++ tib-weivretni/src/main/java/math/HammingDistance.java
@@ -2,6 +2,32 @@
 
 package math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HammingDistance {
+	public static final int modulo = 1000000007;
 	
+	public static int findHammingDistance(final List<Integer> list){
+		int sum = 0;
+		int listSize = list.size();
+		for(int i = 0; i < 31; i++){
+			int setBitcount = 0;
+			for(int j = 0; j < listSize; j++){
+				int num = list.get(j);
+				setBitcount += (num & (1 << i)) != 0 ? 1 : 0;
+			}
+			//{1,1}, {1,2} * 2 = {1,1}, {1,2}, {2,2}, {2,1} 
+			sum += setBitcount * (listSize - setBitcount) * 2;
+			sum = sum % modulo;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		System.out.println(findHammingDistance(list));
+	}
 }
