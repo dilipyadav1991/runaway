62539da1488fabd3424655e73df4bdcd101b6539
Dilip Yadav <dyadav@reputation.com>
2018-05-09 11:20:25 +0530

    faster code

--- tib-weivretni/src/main/java/Array/MaxNonNegativeSubArray.java
+++ tib-weivretni/src/main/java/Array/MaxNonNegativeSubArray.java
@@ -5,34 +5,15 @@ import java.util.ArrayList;
 public class MaxNonNegativeSubArray {
 	public static int sizeOfMaxNonNegativeSubArray(final ArrayList<Integer> list) {
 		int sizeOfMaxNonNegativeSubArray = 0;
-		int i = 0;
-		int j = 0;
 		int listSize = list.size();
-		if (listSize == 0) {
-			return 0;
-		} else if (listSize == 1) {
-			if (list.get(0) < 0) {
-				return 0;
-			} else {
-				return 1;
+		for (int i = 0; i < listSize; i++) {
+			int currentCount = 0;
+			while (i < listSize && list.get(i) >= 0) {
+				currentCount++;
+				i++;
 			}
-		}
-		boolean firstTime = true;
-		for (int index = 0; index < listSize; index++) {
-			int num = list.get(index);
-			if (num >= 0 && firstTime) {
-				i = index;
-				System.out.printf("i = %d\n", i);
-				j = index;
-				firstTime = false;
-			} else if (num >= 0) {
-				j = index;
-				System.out.printf("j = %d\n", j);
-			} else {
-				firstTime = true;
-			}
-			sizeOfMaxNonNegativeSubArray = sizeOfMaxNonNegativeSubArray > (j - i + 1) ? sizeOfMaxNonNegativeSubArray
-					: (j - i + 1);
+			sizeOfMaxNonNegativeSubArray = sizeOfMaxNonNegativeSubArray > currentCount ? sizeOfMaxNonNegativeSubArray
+					: currentCount;
 		}
 		return sizeOfMaxNonNegativeSubArray;
 	}
@@ -41,6 +22,8 @@ public class MaxNonNegativeSubArray {
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(2);
 		list.add(3);
+		list.add(4);
+		list.add(4);
 		list.add(4);
 		list.add(-1);
 		list.add(-2);
