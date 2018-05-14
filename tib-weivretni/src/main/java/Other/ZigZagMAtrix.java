451076021e3644eb50b9464e20d639757b56a41d
Dilip Yadav <dyadav@reputation.com>
2018-05-14 10:00:56 +0530

    zig zag matrix

---
+++ tib-weivretni/src/main/java/Other/ZigZagMAtrix.java
@@ -0,0 +1,43 @@
+package Other;
+
+public class ZigZagMAtrix {
+    public static int R,C;
+
+	public static void main(String[] args) {
+        int arr[][] = { {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16},
+                        {17, 18, 19, 20}, };
+         
+        R=arr.length;
+        C=arr[0].length;
+         
+        diagonalOrder(arr);
+  }
+
+	private static void diagonalOrder(int[][] arr) {
+		//First half diagonal element
+		for(int i = 0; i < R; i++){
+			int row = i;
+			int column = 0;
+			while(row >= 0 && column < C){
+				System.out.print(arr[row][column] + " ");
+				row--;
+				column++;
+			}
+			System.out.println();
+		}
+		//First half diagonal element
+		for(int j = 1; j < C; j++){
+			int row = R - 1;
+			int column = j;
+			while(row >= 0 && column < C){
+				System.out.print(arr[row][column] + " ");
+				row--;
+				column++;
+			}
+			System.out.println();
+		}
+	}
+}
