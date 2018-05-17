21165af8d7900b3b5f195f1f2e788dd6cdb4b838
Dilip Yadav <dyadav@reputation.com>
2018-05-17 08:59:42 +0530

    find duplicates

---
+++ tib-weivretni/src/main/java/Array/DuplicatesInArray.java
@@ -0,0 +1,27 @@
+//https://www.geeksforgeeks.org/duplicates-array-using-o1-extra-space-set-2/
+
+package Array;
+
+import java.util.ArrayList;
+
+public class DuplicatesInArray {
+	public static ArrayList<Integer> findDuplicate(int a[]) {
+		ArrayList<Integer> result = new ArrayList<>();
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int index = a[i] % n;
+			a[index] = a[index] + n;
+		}
+		for (int i = 0; i < n; i++) {
+			if (a[i] / n > 1) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(findDuplicate(new int[] { 1, 1, 1, 2, 2 }));
+	}
+
+}
