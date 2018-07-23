e1f1109e079936c71d6c8d0cbfd4ea553c30c8dd
Dilip Yadav <dyadav@reputation.com>
2018-07-23 11:08:49 +0530

    Max Occuring char.

---
+++ tib-weivretni/src/main/java/String/MaxAccuringCharacter.java
@@ -0,0 +1,29 @@
+//https://www.geeksforgeeks.org/return-maximum-occurring-character-in-the-input-string/
+
+package String;
+
+public class MaxAccuringCharacter {
+	public static void main(String[] args) {
+		String str = "geeksforgeeks";
+		maxOccuringChar(str);
+	}
+
+	public static void maxOccuringChar(String str) {
+		char result = 0;
+		int strLen = str.length();
+		int[] count = new int[256];
+		char[] charArray = str.toCharArray();
+		for (int i = 0; i < strLen; i++) {
+			count[charArray[i]]++;
+		}
+
+		int max = -1;
+		for (int i = 0; i < strLen; i++) {
+			if (count[charArray[i]] > max) {
+				max = count[charArray[i]];
+				result = charArray[i];
+			}
+		}
+		System.out.println("Max Occuring character: " + result);
+	}
+}
