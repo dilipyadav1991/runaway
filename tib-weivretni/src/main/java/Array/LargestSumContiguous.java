ec29ecf683ab8828a6e7daba9235f1f4aa085a30
Dilip Yadav <dyadav@reputation.com>
2018-08-29 07:13:44 +0530

    Largest Sum Contiguous Subarray

--- tib-weivretni/src/main/java/Array/LargestSumContiguous.java
+++ tib-weivretni/src/main/java/Array/LargestSumContiguous.java
@@ -3,22 +3,40 @@
 
 package Array;
 
+import java.util.ArrayList;
+
 public class LargestSumContiguous {
-	public static int findLargestSumContiguous(int a[]) {
-		int maxSoFar = a[0];
-		int currentMax = a[0];
-		for (int i = 1; i < a.length; i++) {
-			int sum = currentMax + a[i];
-			currentMax = sum > a[i] ? sum : a[i];
-			maxSoFar = maxSoFar > currentMax ? maxSoFar : currentMax;
+	public static ArrayList<Integer> findLargestSumContiguous(int a[]) {
+		ArrayList<Integer> list = new ArrayList<>();
+		int start = 0;
+		int end = 0;
+		int x = 0;
+		int maxSoFar = Integer.MIN_VALUE;
+		int currentMax = 0;
+		for (int i = 0; i < a.length; i++) {
+			currentMax += a[i];
+			if (maxSoFar < currentMax) {
+				maxSoFar = currentMax;
+				start = x;
+				end = i;
+			}
+			if (currentMax < 0) {
+				currentMax = 0;
+				x = i + 1;
+			}
 		}
-		return maxSoFar;
+		System.out.println("Maximum contiguous sum is " + maxSoFar);
+		System.out.println("Starting index " + start);
+		System.out.println("Ending index " + end);
+		for (int i = start; i <= end; i++) {
+			list.add(a[i]);
+		}
+		return list;
 	}
 
 	public static void main(String[] args) {
 		int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-		int n = a.length;
-		int max_sum = findLargestSumContiguous(a);
+		ArrayList<Integer> max_sum = findLargestSumContiguous(a);
 		System.out.println("Maximum contiguous sum is " + max_sum);
 	}
 }
