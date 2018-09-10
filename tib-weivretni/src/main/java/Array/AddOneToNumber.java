01b734037e8ce60fa7684c1080db209f152b5ad4
Dilip Yadav <dyadav@reputation.com>
2018-09-10 20:19:21 +0530

    Added array list implementation

--- tib-weivretni/src/main/java/Array/AddOneToNumber.java
+++ tib-weivretni/src/main/java/Array/AddOneToNumber.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.ArrayList;
+
 public class AddOneToNumber {
 	int a[] = { 0, 8, 9 };
 
@@ -29,9 +31,9 @@ public class AddOneToNumber {
 			}
 		} else {
 			for (int i = 0; i < a.length; i++) {
-				if(i == 0){
-					//remove all leading zeros
-					while(a[i] == 0){
+				if (i == 0) {
+					// remove all leading zeros
+					while (a[i] == 0) {
 						i++;
 					}
 				}
@@ -40,8 +42,43 @@ public class AddOneToNumber {
 		}
 	}
 
+	// Array list implementation
+	private void plusOne(ArrayList<Integer> list) {
+		
+		int size = list.size();
+		list.set(size - 1, list.get(size - 1) + 1);
+		int carry = list.get(size - 1) / 10;
+		list.set(size - 1, list.get(size - 1) % 10);
+		
+		for(int i = size - 2; i >= 0 ; i--){
+			if(carry == 1){
+				list.set(i, list.get(i) + carry);
+				carry = list.get(i) / 10;
+				list.set(i, list.get(i) % 10);
+			}
+		}
+		if(carry == 1){
+			list.add(0, 1);
+		}
+		int index = 0;
+		if(list.get(index) == 0 && index == 0){
+			list.remove(index);
+		}
+		System.out.println(list);
+	}
+
 	public static void main(String[] args) {
 		AddOneToNumber addOne = new AddOneToNumber();
-		addOne.addOne();
+		// Array implementation
+		//addOne.addOne();
+
+		// List implementation
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(0);
+		list.add(0);
+		list.add(9);
+		addOne.plusOne(list);
+
 	}
+
 }
