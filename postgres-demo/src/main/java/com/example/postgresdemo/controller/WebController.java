e98bf8431b6bb4a4e0ff07ecc3194a5c17f84f75
Dilip Yadav <dyadav@reputation.com>
2018-10-06 12:05:00 +0530

    Saving student to db

--- postgres-demo/src/main/java/com/example/postgresdemo/controller/WebController.java
+++ postgres-demo/src/main/java/com/example/postgresdemo/controller/WebController.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class WebController {
 	@RequestMapping("/save")
 	public String process() {
 		// save a single Customer
-		repository.save(new Student("001", "Jack", "Smith"));
+		repository.save(new Student("RIPL009","Dilip", "Yadav"));
 		return "Done";
 	}
 
@@ -28,5 +30,19 @@ public class WebController {
 		Optional<Student> findbyid = repository.findById(id);
 		Student student = findbyid.get();
 		return student.getName();
+	}
+
+	@PostMapping(value = "/saveStudent")
+	public Student createStudent(@RequestBody Student student) {// @RequestBody
+																// means to
+																// parse JSON
+																// data into map
+																// or java beans
+																// and only
+																// support
+																// content type
+																// is
+																// "application/json;charset=UTF-8"
+		return repository.save(student);
 	}
 }
--- postgres-demo/src/main/java/com/example/postgresdemo/model/Student.java
+++ postgres-demo/src/main/java/com/example/postgresdemo/model/Student.java
@@ -6,25 +6,50 @@ import javax.persistence.Id;
 
 @Entity
 public class Student {
-	
+
 	@Id
 	private String id;
-	
+
 	@Column
 	private String firstName;
-	
+
 	@Column
 	private String lastName;
-	
-	public Student(){}
-	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Student() {
+	}
+
 	public Student(String id, String firstName, String lastName) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return firstName + " " + lastName;
 	}
 }
