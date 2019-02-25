5d460eebe8321867adf6da35eaa9e5539b597df3
Dilip Yadav <dyadav@reputation.com>
2019-02-25 19:55:36 +0530

    initial commit

---
+++ tib-weivretni/src/main/java/GenericDataStructure/stack/LinkedStack.java
@@ -0,0 +1,28 @@
+package GenericDataStructure.stack;
+
+public class LinkedStack<T> implements Stack<T> {
+	
+
+	@Override
+	public void push(T data) {
+		if(!empty()) {
+			
+		}
+	}
+
+	@Override
+	public T pop() {
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		return null;
+	}
+
+	@Override
+	public boolean empty() {
+		return false;
+	}
+
+}
