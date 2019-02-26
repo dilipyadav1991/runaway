d248195e525a74e01e30bca9c7211dd754083ec9
Dilip Yadav <dyadav@reputation.com>
2019-02-26 15:50:03 +0530

    Generic linked list

---
+++ tib-weivretni/src/main/java/GenericDataStructure/linkedlist/LinkedList.java
@@ -0,0 +1,53 @@
+package GenericDataStructure.linkedlist;
+
+public class LinkedList<T> {
+	Node<T> head = null;
+
+	public void add(T data) {
+		Node<T> temp = head;
+		if (head == null) {
+			head = new Node<T>(data);
+		} else {
+			Node<T> node = new Node<T>(data);
+			while (temp.getNext() != null) {
+				temp = temp.getNext();
+			}
+			temp.setNext(node);
+		}
+	}
+
+	public void remove(T data) {
+		Node<T> temp = head;
+		if (head.getData() == data) {
+			head = head.getNext();
+		}
+		while (temp != null) {
+			if (temp.getNext() != null && temp.getNext().getData() == data) {
+				temp.setNext(temp.getNext().getNext());
+			}
+			temp = temp.getNext();
+		}
+	}
+
+	public void print() {
+		Node<T> temp = head;
+		while (temp != null) {
+			System.out.print(temp.getData());
+			temp = temp.getNext();
+			System.out.print(" -> ");
+		}
+		System.out.println(" null ");
+	}
+
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.print();
+		list.remove("Binod");
+		list.print();
+
+	}
+}
